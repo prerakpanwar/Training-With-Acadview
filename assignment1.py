37bbbd25494e58ab7bb061b101e595cf11ef9c51
prerakpanwar <panwarprerak98@gmail.com>
2018-06-07 05:44:06 -0700

    pehli file

---
+++ assignment1.py
@@ -0,0 +1,33 @@
+print("Q.1- Print anything you want on screen. ")
+
+print("ASSINGMENT 1\n\n\n")
+
+print("Q.2- Join two strings using '+' E.g.-'Acad'+'View'.")
+
+print("Acad"+"View\n\n\n")
+
+print("Q.3- Take the input of 3 variables x, y and z . Print their values on screen.") 
+
+x = input("Enter x : ")
+y = input("Enter y : ")
+z = input("Enter z : ")
+print("Values of x , y and z are "+ x +" , "+ y +" and "+ z +" respectively.\n\n\n")
+
+print("Q.4- Print “Let’s get started” on screen.") 
+
+print("Let's get started!\n\n\n")
+
+print("Q.5- Print the following values using placeholders. s=”Acadview” course=”Python” fees=5000.")
+
+s="Acadview"
+course="Python"
+fees = 5000
+print("s = " + s)
+print("course = " + course)
+print("fees = " + str(fees) + "\n\n\n")
+
+print("Q.6- Let’s do some interesting exercise: name=”Tony Stark”   salary=1000000   print(‘%s’’%d’)%(____ ,____).")
+
+name="Tony Stark"
+salary=1000000
+print(name + "  " + str(salary))
